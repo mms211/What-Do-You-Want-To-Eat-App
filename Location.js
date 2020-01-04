a425851f97681074158d60e7fd2570b567e384ce
Colin Cunningham <colin.199643@gmail.com>
2020-01-04 13:23:30 -0500

    Included Zomato API to pull 'curated' resturants based on location

--- Location.js
+++ Location.js
@@ -4,6 +4,10 @@ var ipAddress = localStorage.address
 
 var getLocation = "https://api.ipgeolocation.io/ipgeo?" + apiKey + ipAddress 
 
+var lat = localStorage.latitude
+
+var lon = localStorage.longitude
+
 
 $(function getIP() {
     $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
@@ -22,8 +26,29 @@ $.ajax({
     })
 
 .then(function(response){
-    console.log("Latitude: " + response.latitude)
-    console.log("Longitude: " + response.longitude)
-
+    
+    console.log(response.latitude)
+  
+    localStorage.latitude = response.latitude
+    localStorage.longitude = response.longitude
 
 });
+
+function generateAPI() {
+  $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      beforeSend: function(response) {
+        response.setRequestHeader('user-key', '736fc2557bb7bf4c8f77bc93b98b55f0');
+      },
+      url: "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + lon,
+      success: function(response) {
+      console.log(response)
+  },
+  error: function(response) {
+      
+    
+    }
+});
+}
+generateAPI();
